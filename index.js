fb8f99da78f10611a505cf8f4ff9ba59418cc88d
Esteban Barrón <ebarronteodoro@hotmail.com>
2023-05-09 04:59:09 -0500

    git add .

---
+++ index.js
@@ -0,0 +1,16 @@
+// const element = document.getElementById("Esteban");
+// const borderRadii = ["25% 75% 25% 75% / 75% 25% 75% 25% ", "50% 50% 50% 50% / 50% 50% 50% 50%", "75% 25% 75% 25% / 25% 75% 25% 75%", "49% 51% 48% 52% / 51% 48% 52% 49% "];
+// let index = 0;
+
+// function changeBorderRadius() {
+//   element.style.borderRadius = borderRadii[index];
+
+//   index++;
+//   if (index >= borderRadii.length) {
+//     index = 0;
+//   }
+
+//   setTimeout(changeBorderRadius, 2000);
+// }
+
+// changeBorderRadius();
