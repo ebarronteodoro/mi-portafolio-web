f3d0b3232faf239b8376e4f0222728e723300e87
Esteban Barrón <ebarronteodoro@hotmail.com>
2023-05-10 08:27:26 -0500

    hamburguer 1

--- index.js
+++ index.js
@@ -41,53 +41,75 @@ addEventListener("DOMContentLoaded", () => {
   // }
 
   // Evento de clic en los enlaces de navegación
-document.querySelectorAll('header nav a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
+  document.querySelectorAll("header nav a").forEach(function (link) {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
-  
-      var targetSectionId = this.getAttribute('href');
+
+      var targetSectionId = this.getAttribute("href");
       var targetSection = document.querySelector(targetSectionId);
-  
-      document.querySelectorAll('header nav a').forEach(function(navLink) {
+
+      document.querySelectorAll("header nav a").forEach(function (navLink) {
         if (navLink === event.target) {
-          navLink.classList.add('active');
+          navLink.classList.add("active");
         } else {
-          navLink.classList.remove('active');
+          navLink.classList.remove("active");
         }
       });
-  
+
       window.scrollTo({
-        top: targetSection.offsetTop - document.querySelector('header').offsetHeight,
-        behavior: 'smooth'
+        top:
+          targetSection.offsetTop -
+          document.querySelector("header").offsetHeight,
+        behavior: "smooth",
       });
     });
   });
-  
+
   // Evento de desplazamiento (scroll)
-  window.addEventListener('scroll', function() {
-    var headerHeight = document.querySelector('header').offsetHeight;
-    var sections = document.querySelectorAll('section');
-    var navLinks = document.querySelectorAll('header nav a');
-  
+  window.addEventListener("scroll", function () {
+    var headerHeight = document.querySelector("header").offsetHeight;
+    var sections = document.querySelectorAll("section");
+    var navLinks = document.querySelectorAll("header nav a");
+
     for (var i = 0; i < sections.length; i++) {
       var section = sections[i];
       var sectionTop = section.offsetTop;
-  
+
       if (window.pageYOffset >= sectionTop - headerHeight) {
         for (var j = 0; j < navLinks.length; j++) {
           var link = navLinks[j];
-          if (link.getAttribute('href') === '#' + section.getAttribute('id')) {
-            link.classList.add('active');
+          if (link.getAttribute("href") === "#" + section.getAttribute("id")) {
+            link.classList.add("active");
           } else {
-            link.classList.remove('active');
+            link.classList.remove("active");
           }
         }
-        section.style.paddingTop = '0';
+        section.style.paddingTop = "0";
       } else {
-        section.style.paddingTop = '0';
+        section.style.paddingTop = "0";
       }
     }
   });
-  
-  
+
+  var hamburgerIcon = document.querySelector(".hamburger-icon");
+  var nav = document.querySelector("header div nav");
+
+  hamburgerIcon.addEventListener("click", function () {
+    nav.classList.toggle("deployed");
+
+    if (nav.classList.contains("deployed")) {
+      setTimeout(function () {
+        nav.querySelector("nav").style.opacity = "1";
+      }, 10);
+    } else {
+      nav.querySelector("nav").style.opacity = "0";
+    }
+  });
+
+  var navEnlaces = document.querySelectorAll("header div nav ul li a");
+  navEnlaces.forEach(function (link) {
+    link.addEventListener("click", function () {
+      nav.classList.remove("deployed");
+    });
+  });
 });
